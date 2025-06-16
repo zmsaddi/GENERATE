994d133f3e04e64ddf11713f894339306ba08b4f
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-06-16 14:23:59 +0200

    Delete js directory

--- js/canvas-render.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('exportBtn').addEventListener('click', () => {
-  const dataURL = canvas.toDataURL({ format: 'png', quality: 1 });
-  const link = document.createElement('a');
-  link.href = dataURL; link.download = 'poster.png'; link.click();
-});
