896e4a24794b0e4cdc0b608ecfac385c76cc6ecb
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-06-16 15:20:15 +0200

    Delete js directory

--- js/fabric-init.js
+++
@@ -1,8 +0,0 @@
-const canvas = new fabric.Canvas('posterCanvas', { preserveObjectStacking: true });
-function initCanvas(size) {
-  const [w, h] = size.split('x').map(Number);
-  canvas.setWidth(w).setHeight(h);
-  canvas.backgroundColor = '#001c40';
-  canvas.renderAll();
-}
-initCanvas('1080x1350');
